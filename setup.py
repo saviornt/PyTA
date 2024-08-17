ec03fd59e2812ade74e61c52904e1eea4054b5a7
saviornt <david.wadsworth80@gmail.com>
2024-08-17 02:27:37 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PyTA",
-    version="0.1.0",
+    version="0.1.1",
     description="PyTA is a Python library for technical analysis, offering a range of functions to compute indicators like moving averages, momentum, volatility, and patterns. Designed as a user-friendly alternative to TA-Lib, it leverages pandas and numpy for ease of use.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
